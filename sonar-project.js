0acd89e8a083e920497f6e41284327ad551f0144
SHAIK MABASHA DEVOPS WITH AWS <165106898+mabashagithub@users.noreply.github.com>
2024-09-19 09:54:45 +0530

    Add files via upload

---
+++ sonar-project.js
@@ -0,0 +1,19 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: 'http://54.254.243.193:9000/',
+       options : {
+	    'sonar.projectDescription': 'This is a Node JS application',
+	    'sonar.projectName': 'Node JS Application - Sample',
+	    'sonar.projectKey':'NodeJsMithunTechnologies',
+	    'sonar.login': 'squ_e2237ad77934d4fe7f6be147c53819acb1079580',
+	    //'sonar.login': 'admin',
+	    //'sonar.password': 'admin',
+            'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	  //'sonar.tests': 'specs',
+          //'sonar.inclusions' : 'src/**'
+       },
+}, () => {});
